88c3eb4b88e647e7924e64d67a100f1e17913998
Diego <diego.maquill@gmail.com>
2019-08-06 16:09:28 -0400

    fix20

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 // Exporting an object containing all of our models
+// var Article = require('./article');
+// var Note = require('./note');
 
 module.exports = {
-    Article: require("./Article"),
-    Note: require("./Note")
-  };
+  Article: require("./article"),
+  Note: require("./note")
+};
